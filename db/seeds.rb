6eda34275d081f6295486bfb0c0e3b07159a584a
Andrew Grant <andrewgrant2009@hotmail.com>
2022-10-25 19:51:49 +0200

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Cleaning the database 🧹"
+Pet.destroy_all
+puts "Here are some new sweet-pets"
+
+20.times do
+  Pet.create(
+    name: Faker::FunnyName.two_word_name,
+    city: "Berlin",
+    species: ["dog", "cat", "piglet", "monkey", "rabbit"].sample,
+    age: ["1", "2", "3", "4", "9", "12", "16"].sample,
+    category: ["Sassy", "Funny", "Needy", "Mischievous"].sample,
+    postcode: Faker::Address.postcode,
+    description: "lovely pet, very friendly"
+  )
+end
