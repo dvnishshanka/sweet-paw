1f741c07e4a68fd6ef4cfe9b58b1f78ff628c4e5
sara pulido <sara01rizo@gmail.com>
2022-10-26 11:57:07 +0200

    Update: pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,19 @@
 class PetsController < ApplicationController
+  skip_before_action :authenticate_user!, :pet_params, only: %i[index show]
   def new
   end
 
   def index
     @pets = Pet.all
+  end
+
+  def show
+    @oet = Pet.find(params[:id])
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :description, :photo)
   end
 end
