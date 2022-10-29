fb2caea5929d50bdd5ea4246a37aa69d0ecb1316
Andrew Grant <andrewgrant2009@hotmail.com>
2022-10-29 14:39:38 +0200

    updating

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -21,8 +21,8 @@ class PetsController < ApplicationController
       {
         lat: pet.latitude,
         lng: pet.longitude,
-        info_window: render_to_string(partial: "info_window", locals: {pet: pet}),
-        image_url: helpers.asset_url("../assets/images/monkey.png")
+        info_window: render_to_string(partial: "shared/info_window", locals: {pet: pet}),
+        image_url: helpers.asset_url("dog.png")
       }
     end
   end
