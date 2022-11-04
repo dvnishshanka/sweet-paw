ec7d478f252362a00bfd6e12093a035cdd662d4a
Andrew Grant <andrewgrant2009@hotmail.com>
2022-11-04 11:25:42 +0100

    Added stimulus flatpickr

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,16 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
+
 
 const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+
+// import Flatpickr
+import Flatpickr from "stimulus-flatpickr"
+
+application.register('flatpickr', Flatpickr)
 
 // Configure Stimulus development experience
 application.debug = false
